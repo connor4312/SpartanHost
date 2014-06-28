e1bada41d2cbc9ef7c24a2830f86b3e563799ab2
cuddylier <ryanandross@hotmail.co.uk>
2014-06-28 01:19:29 +0100

    Changed status page URL

--- multicraft/protected/views/server/view.php
+++ multicraft/protected/views/server/view.php
@@ -288,7 +288,7 @@ else
         $con = McBridge::get()->getConnection($model->daemon_id);
         if ($con)
             $attribs[] = array('label'=>$form->labelEx($model,'daemon_id'), 'type'=>'raw',
-                'value'=>CHtml::link($con->name, 'http://status.spartanhost.net/status', array('target'=>'_blank')));
+                'value'=>CHtml::link($con->name, 'http://status.spartanhost.net', array('target'=>'_blank')));
     }
     if (Yii::app()->user->isSuperuser() || $settings->user_players)
     {
